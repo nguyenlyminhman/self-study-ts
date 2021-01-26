a69c5a9070abf058e62337f986255f43dd6527f7
Man Nguyen <nguyenlyminhman@gmail.com>
2021-01-26 22:02:00 +0700

    class

---
+++ class/index.js
@@ -0,0 +1,12 @@
+var GameObject = /** @class */ (function () {
+    function GameObject(name, slogan, energy) {
+        this._name = name;
+        this._slogan = slogan;
+        this._energy = energy;
+    }
+    GameObject.prototype.run = function () {
+    };
+    GameObject.prototype.cut = function () {
+    };
+    return GameObject;
+}());
---
+++ class/index.ts
@@ -0,0 +1,20 @@
+class GameObject {
+    private _name : String;
+    private _slogan: String;
+    private _energy: number;
+
+    constructor(name: String, slogan : String, energy : number){
+        this._name = name;
+        this._slogan = slogan;
+        this._energy = energy;
+    }
+
+    run(){
+
+    }
+
+    cut(){
+
+    }
+    
+}
